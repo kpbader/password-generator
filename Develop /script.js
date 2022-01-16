69f2bee5dab428c3efd4a22ff6d3fac0b59567d1
Kyle Patrick Bader <kbader91@gmail.com>
2022-01-16 10:47:13 -0800

    added tempStr, For loops, and slice

--- Develop /script.js
+++ Develop /script.js
@@ -39,11 +39,20 @@ function generatePassword() {
 
  // User chooses correct amount of characters
 if (length >= 8 && length <= 128) {
+  
     // if user chooses all options...
     if (isLower === true && isUpper === true && isNumeric === true && isSpecial === true) {
        setPassword += randomLower + randomUpper + randomNumeric + randomSpecial;
-       return setPassword;
+       // append to the temp variable which will be our password a random character
+       let tempStr = "";
+       // to the length of the password, with a random character from the setPassword variable 
+       for (var index = 0; index < length; index++) {
+         tempStr += setPassword.charAt(Math.floor(Math.random() * setPassword.length))
+       };
+       // cut password to the length of the user input 
+       return tempStr.slice(0, length);
       }
+
     // // if user chooses no options 
     // else {
     //   window.alert("You must choose at least one option!");
@@ -53,78 +62,183 @@ if (length >= 8 && length <= 128) {
     // if user chooses uppercase, numbers, and special characters...
     if (isLower === false && isUpper === true && isNumeric === true && isSpecial === true) {
       setPassword += randomUpper + randomNumeric + randomSpecial;
-      return setPassword;
+      // append to the temp variable which will be our password a random character
+       let tempStr = "";
+       // to the length of the password, with a random character from the setPassword variable 
+       for (var index = 0; index < length; index++) {
+         tempStr += setPassword.charAt(Math.floor(Math.random() * setPassword.length))
+       };
+       // cut password to the length of the user input 
+       return tempStr.slice(0, length);
     } 
+
     // lowercase only
     if (isLower === true && isUpper === false && isNumeric === false && isSpecial === false) {
       setPassword += randomLower;
-      return setPassword;
+      // append to the temp variable which will be our password a random character
+       let tempStr = "";
+       // to the length of the password, with a random character from the setPassword variable 
+       for (var index = 0; index < length; index++) {
+         tempStr += setPassword.charAt(Math.floor(Math.random() * setPassword.length))
+       };
+       // cut password to the length of the user input 
+       return tempStr.slice(0, length);
     }
 
     // if user chooses numbers and special characters...
     if (isLower === false && isUpper === false && isNumeric === true && isSpecial === true) {
       setPassword += randomNumeric + randomSpecial;
-      return setPassword;
+      // append to the temp variable which will be our password a random character
+       let tempStr = "";
+       // to the length of the password, with a random character from the setPassword variable 
+       for (var index = 0; index < length; index++) {
+         tempStr += setPassword.charAt(Math.floor(Math.random() * setPassword.length))
+       };
+       // cut password to the length of the user input 
+       return tempStr.slice(0, length);
     }
+
     // lowercase and uppercase only 
     if (isLower === true && isUpper === true && isNumeric === false && isSpecial === false) {
       setPassword += randomLower + randomUpper;
-      return setPassword;
+      // append to the temp variable which will be our password a random character
+       let tempStr = "";
+       // to the length of the password, with a random character from the setPassword variable 
+       for (var index = 0; index < length; index++) {
+         tempStr += setPassword.charAt(Math.floor(Math.random() * setPassword.length))
+       };
+       // cut password to the length of the user input 
+       return tempStr.slice(0, length);
     }
 
     // if user chooses only special characters 
     if (isLower === false && isUpper === false && isNumeric === false && isSpecial === true) {
     setPassword += randomSpecial;
-    return setPassword;
+    // append to the temp variable which will be our password a random character
+       let tempStr = "";
+       // to the length of the password, with a random character from the setPassword variable 
+       for (var index = 0; index < length; index++) {
+         tempStr += setPassword.charAt(Math.floor(Math.random() * setPassword.length))
+       };
+       // cut password to the length of the user input 
+       return tempStr.slice(0, length);
     }
+
     // lowercase, uppercase, and numbers only 
     if (isLower === true && isUpper === true && isNumeric === true && isSpecial === false) {
       setPassword += randomLower + randomUpper + randomNumeric;
-      return setPassword;
+      // append to the temp variable which will be our password a random character
+       let tempStr = "";
+       // to the length of the password, with a random character from the setPassword variable 
+       for (var index = 0; index < length; index++) {
+         tempStr += setPassword.charAt(Math.floor(Math.random() * setPassword.length))
+       };
+       // cut password to the length of the user input 
+       return tempStr.slice(0, length);
     }
 
     // uppercase only 
     if (isLower === false && isUpper === true && isNumeric === false && isSpecial === false) {
       setPassword += randomUpper;
-      return setPassword;
+      // append to the temp variable which will be our password a random character
+       let tempStr = "";
+       // to the length of the password, with a random character from the setPassword variable 
+       for (var index = 0; index < length; index++) {
+         tempStr += setPassword.charAt(Math.floor(Math.random() * setPassword.length))
+       };
+       // cut password to the length of the user input 
+       return tempStr.slice(0, length);
     }
+
     // lowercase, number, and special
     if (isLower === true && isUpper === false && isNumeric === true && isSpecial === true) {
       setPassword += randomLower + randomNumeric + randomSpecial;
-      return setPassword;
+      // append to the temp variable which will be our password a random character
+       let tempStr = "";
+       // to the length of the password, with a random character from the setPassword variable 
+       for (var index = 0; index < length; index++) {
+         tempStr += setPassword.charAt(Math.floor(Math.random() * setPassword.length))
+       };
+       // cut password to the length of the user input 
+       return tempStr.slice(0, length);
     }
 
     // number only 
     if (isLower === false && isUpper === false && isNumeric == true && isSpecial == false) {
       setPassword += randomNumeric;
-      return setPassword;
+      // append to the temp variable which will be our password a random character
+       let tempStr = "";
+       // to the length of the password, with a random character from the setPassword variable 
+       for (var index = 0; index < length; index++) {
+         tempStr += setPassword.charAt(Math.floor(Math.random() * setPassword.length))
+       };
+       // cut password to the length of the user input 
+       return tempStr.slice(0, length);
     }
+
     // lowercase, uppercase, and special
     if (isLower === true && isUpper === true && isNumeric === false && isSpecial === true) {
       setPassword += randomLower + randomUpper + randomSpecial;
-      return setPassword;
+      // append to the temp variable which will be our password a random character
+       let tempStr = "";
+       // to the length of the password, with a random character from the setPassword variable 
+       for (var index = 0; index < length; index++) {
+         tempStr += setPassword.charAt(Math.floor(Math.random() * setPassword.length))
+       };
+       // cut password to the length of the user input 
+       return tempStr.slice(0, length);
     }
 
     // uppercase and special 
     if (isLower === false && isUpper === true && isNumeric === false & isSpecial === true) {
       setPassword += randomUpper + randomSpecial;
-      return setPassword;
+      // append to the temp variable which will be our password a random character
+       let tempStr = "";
+       // to the length of the password, with a random character from the setPassword variable 
+       for (var index = 0; index < length; index++) {
+         tempStr += setPassword.charAt(Math.floor(Math.random() * setPassword.length))
+       };
+       // cut password to the length of the user input 
+       return tempStr.slice(0, length);
     }
+
     // lowercase and number 
     if (isLower === true && isUpper === false && isNumeric === true && isSpecial === false) {
       setPassword += randomLower + randomNumeric;
-      return setPassword;
+      // append to the temp variable which will be our password a random character
+       let tempStr = "";
+       // to the length of the password, with a random character from the setPassword variable 
+       for (var index = 0; index < length; index++) {
+         tempStr += setPassword.charAt(Math.floor(Math.random() * setPassword.length))
+       };
+       // cut password to the length of the user input 
+       return tempStr.slice(0, length);
     }
 
     // uppercase and number 
     if (isLower === false && isUpper === true && isNumeric === true && isSpecial === false) {
       setPassword += randomUpper + randomNumeric;
-      return setPassword;
+      // append to the temp variable which will be our password a random character
+       let tempStr = "";
+       // to the length of the password, with a random character from the setPassword variable 
+       for (var index = 0; index < length; index++) {
+         tempStr += setPassword.charAt(Math.floor(Math.random() * setPassword.length))
+       };
+       // cut password to the length of the user input 
+       return tempStr.slice(0, length);
     }
+
     // lowercase and special
     if (isLower === true && isUpper === false && isNumeric === false && isSpecial === true) {
       setPassword += randomLower + randomSpecial;
-      return setPassword;
+      // append to the temp variable which will be our password a random character
+       let tempStr = "";
+       // to the length of the password, with a random character from the setPassword variable 
+       for (var index = 0; index < length; index++) {
+         tempStr += setPassword.charAt(Math.floor(Math.random() * setPassword.length))
+       };
+       // cut password to the length of the user input 
+       return tempStr.slice(0, length);
     }
   }
 };
